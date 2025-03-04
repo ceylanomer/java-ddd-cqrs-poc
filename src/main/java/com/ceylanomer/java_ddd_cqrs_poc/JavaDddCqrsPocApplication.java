dea888ce345742a97d12a6fef5a07ab701a05f4c
omer.ceylan <omer.ceylan@trendyol.com>
2025-03-04 14:18:17 +0300

    first commit

---
+++ src/main/java/com/ceylanomer/java_ddd_cqrs_poc/JavaDddCqrsPocApplication.java
@@ -0,0 +1,13 @@
+package com.ceylanomer.java_ddd_cqrs_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDddCqrsPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDddCqrsPocApplication.class, args);
+	}
+
+}
